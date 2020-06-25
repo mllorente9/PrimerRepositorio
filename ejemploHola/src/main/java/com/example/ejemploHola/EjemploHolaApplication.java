4ab83d3a0b871cb7c3f72b7ab9e3c316c1c553da
miguel <miguel.llorente@bosonit.com>
2020-06-25 17:17:36 +0200

    cambios en el proyecto de hola

--- ejemploHola/src/main/java/com/example/ejemploHola/EjemploHolaApplication.java
+++ ejemploHola/src/main/java/com/example/ejemploHola/EjemploHolaApplication.java
@@ -1,5 +1,4 @@
 package com.example.ejemploHola;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ ejemploHola/src/main/java/com/example/ejemploHola/PrimerEndpoint.java
@@ -0,0 +1,8 @@
+package com.example.ejemploHola;
+
+
+public class PrimerEndpoint {
+public static void main(String args[]) {
+	System.out.println("Hola");
+}
+}
