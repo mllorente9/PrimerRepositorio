2d6ae9486b0242202bac0ef8f972db9f58d21c39
miguel <miguel.llorente@bosonit.com>
2020-06-25 15:55:38 +0200

    Proyecto maven con spring

---
+++ src/main/java/com/example/ejemloHola/EjemloHolaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejemloHola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemloHolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemloHolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ejemloHola/EjemloHolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejemloHola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemloHolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
